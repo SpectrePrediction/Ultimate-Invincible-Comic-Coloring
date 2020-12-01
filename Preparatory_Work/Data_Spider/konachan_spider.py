9cae5eb3c79c9150f10e2faecb73c403e20f2ce5
SpectrePrediction <48947643+SpectrePrediction@users.noreply.github.com>
2020-12-01 22:32:34 +0800

    更新了模型以及爬虫的注释
    模型更加的完善，但还是没有做完，爬虫的注释如约而至，同样的还是有改进的空间 来自SpectrePrediction 12.01

--- Preparatory_Work/Data_Spider/konachan_spider.py
+++ Preparatory_Work/Data_Spider/konachan_spider.py
@@ -10,6 +10,12 @@ picture_root_url = 'https://konachan.net/post?page='
 
 
 def log_print(*args, **kwargs):
+    """
+    注: 在cmd等不支持彩色的命令框 log_print函数无法发挥他颜色的效果
+    :param args: 一帆风顺
+    :param kwargs:  一帆风顺
+    :return:
+    """
     print("\033[0;31m[log:\033[0m", *args, "\033[0;31m]\033[0m", **kwargs)
 
 
@@ -19,6 +25,9 @@ class PictureDownloader:
                  time_out: int=None, header: dict=picture_header, is_show_progress: bool=True):
         """
         PictureDownloader图片下载者
+        通过指定的页数得到所有图片的下载地址
+        然后使用download_to函数将其下载到一个文件夹中
+
         :param star_page:   第几页开始 默认从头开始
         :param num_page:    下载几页 默认下载一页
         注： 包含此页在内，例如第一页开始，num_page为1，则下载第一页结束
@@ -26,6 +35,7 @@ class PictureDownloader:
         :param time_out:    访问超时时间设置，为None则不限
         :param header:      访问头，使用默认即可
         :param is_show_progress:  是否展示进度条
+        注: 在cmd等不支持彩色的命令框 log_print函数无法发挥他颜色的效果
         """
 
         self.star_page = star_page if star_page >= 0 else 0
@@ -42,6 +52,11 @@ class PictureDownloader:
         log_print("PictureDownloader 已退出")
 
     def get_picture_url_list(self) -> list:
+        """
+        get_picture_url_list得到图片下载地址的列表
+        他通过构造函数自动执行并获取，同样是后面下载的必需品
+        :return: 图片下载地址的列表
+        """
 
         progress = self.show_progress(self.end_page - self.star_page)
 
@@ -72,10 +87,23 @@ class PictureDownloader:
 
     @staticmethod
     def save_picture(picture_data: bytes, save_path: str):
+        """
+        保存图片（注：早期中判断已被移出到其他地方，现在save_picture只负责保存）
+        :param picture_data: 图片数据
+        :param save_path: 保存地址
+        :return: None
+        """
         with open(save_path, 'wb') as picture:
             picture.write(picture_data)
     
     def download_to(self, save_root_path: str, is_cover: bool=False):
+        """
+        下载到指定文件夹
+        :param save_root_path: 保存图片的根目录（文件夹）
+        注： 可以不带‘/’，例如"."与"./"是一致的
+        :param is_cover: 如果图像存在，是否覆盖 默认否
+        :return: None
+        """
 
         if not save_root_path.endswith('/'):
             save_root_path += '/'
@@ -116,4 +144,4 @@ class PictureDownloader:
 
 
 pd = PictureDownloader(star_page=0, num_page=20, is_show_progress=True)
-pd.download_to("./downimg")
+pd.download_to("./downimg")  # 应当有此目录，程序并不会自动创建
